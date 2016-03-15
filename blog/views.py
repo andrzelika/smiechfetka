1774654b81b2775a687173bd239aa6aebdcf3153
andrzelika <smiechfetka@gmail.com>
2016-03-15 22:59:58 +0100

    pierwsza wersja widoku strony głównej

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    # stworzenie zmiennej dla QuerySetu
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # metoda przyjmuje zapytanie i zwraca metodę render renderującą szablon html
+    # przekazujemy QuerySet z listą posów do szablonu w zmiennej 'posts'
+    return render(request, 'blog/post_list.html', {'posts': posts})
--- smiechsite/settings.py
+++ smiechsite/settings.py
@@ -88,16 +88,20 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, "static"),
+)
